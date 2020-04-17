9fcf5c9cc9ee63aaebcaef5253881a012232cce2
NicoBrg-WDG <61825049+NicoBrg-WDG@users.noreply.github.com>
2020-04-17 21:23:31 +0200

    Update constants.groovy

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -1,23 +1,24 @@
 class constants {
         environments = [
-        dev: [
-                beanstalkEnv: 'DEVELOPMENT',
-                frontConfig: 'dev'
-        ],
-        devstaging: [
-                beanstalkEnv: 'DEV-STAGING',
-                frontConfig: 'dev-staging'
-        ],
-        demo: [
-                beanstalkEnv: 'DEMO',
-                frontConfig: 'demo'
-        ],
-        prod: [
-                beanstalkEnv: 'PRODUCTION',
-                frontConfig: 'prod'
+                dev: [
+                        beanstalkEnv: 'DEVELOPMENT',
+                        frontConfig: 'dev'
+                ],
+                devstaging: [
+                        beanstalkEnv: 'DEV-STAGING',
+                        frontConfig: 'dev-staging'
+                ],
+                demo: [
+                        beanstalkEnv: 'DEMO',
+                        frontConfig: 'demo'
+                ],
+                prod: [
+                        beanstalkEnv: 'PRODUCTION',
+                        frontConfig: 'prod'
+                ]
         ]
-}
+        def getEnvironment(name) {
+                return environments.get(name)
+        }
 
-def getEnvironment(name) {
-        return environments.get(name)
 }
