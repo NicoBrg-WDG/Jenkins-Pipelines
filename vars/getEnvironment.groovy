dc01c0b7b6e85cbf8b01981d763c7be4ad37a18e
NicoBrg-WDG <61825049+NicoBrg-WDG@users.noreply.github.com>
2020-04-17 22:02:22 +0200

    Update and rename Constants.groovy to getEnvironment.groovy

---
+++ vars/getEnvironment.groovy
@@ -0,0 +1,21 @@
+def call(String name)
+        environments = [
+                dev: [
+                        beanstalkEnv: 'DEVELOPMENT',
+                        frontConfig: 'dev'
+                ],
+                devstaging: [
+                        beanstalkEnv: 'DEV-STAGING',
+                        frontConfig: 'dev-staging'
+                ],
+                demo: [
+                        beanstalkEnv: 'DEMO',
+                        frontConfig: 'demo'
+                ],
+                prod: [
+                        beanstalkEnv: 'PRODUCTION',
+                        frontConfig: 'prod'
+                ]
+        ]
+        return environments.get(name)
+}
