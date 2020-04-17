eae2ae588b81051a13466688c752e23eb24a7c6c
NicoBrg-WDG <61825049+NicoBrg-WDG@users.noreply.github.com>
2020-04-17 15:45:01 +0200

    Update sub-pipeline.groovy

--- sub-pipeline.groovy
+++ sub-pipeline.groovy
@@ -4,15 +4,19 @@ pipeline {
       stage ('sub pipeline'){
          parallel{
             stage('sub pipeline stage 1') {
+               agent any
                echo 'sub pipeline stage 1 executed'
             }
             stage('sub pipeline stage 2') {
+               agent any
                echo 'sub pipeline stage 2 executed'
             }
             stage('sub pipeline stage 3') {
+               agent any
                echo 'sub pipeline stage 3 executed'
             }
             stage('sub pipeline stage 4') {
+               agent any
                echo 'sub pipeline stage 4 executed'
             }
          }
