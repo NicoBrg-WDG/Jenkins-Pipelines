57e3d0e6c3e014cf86d57b8bac221d84b3832e1d
NicoBrg-WDG <61825049+NicoBrg-WDG@users.noreply.github.com>
2020-04-17 22:26:23 +0200

    Update parent-pipeline.groovy

--- parent-pipeline.groovy
+++ parent-pipeline.groovy
@@ -7,9 +7,11 @@ pipeline {
                 echo 'PARENT PIPELINE RUNNING'
                 echo getEnvironment('dev').toString()
                 echo 'CALLING SUB PIPELINE'
+                script{
                 some_var = build job: 'Child Job'
                 log = Jenkins.getInstance().getItemByFullName('Child Job').getBuildByNumber(some_var.getNumber()).logFile.text
-                echo log
+                print log
+                }
                 echo 'SUB PIPELINE FINISHED'
             }
         }
