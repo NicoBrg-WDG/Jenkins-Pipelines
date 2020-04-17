c060ce5a5e433c06d5b4ee0d81271f0334d8063d
NicoBrg-WDG <61825049+NicoBrg-WDG@users.noreply.github.com>
2020-04-17 16:26:38 +0200

    Update environments.groovy

--- vars/environments.groovy
+++ vars/environments.groovy
@@ -4,30 +4,18 @@ def getEnvironments() {
 return [
         dev: [
                 beanstalkEnv: 'DEVELOPMENT',
-                frontConfig: 'dev',
-                frontGiftBucket: 'gift-front-dev-gift',
-                frontAdminBucket: 'gift-front-dev-admin',
-                notify: false
+                frontConfig: 'dev'
         ],
         devstaging: [
                 beanstalkEnv: 'DEV-STAGING',
-                frontConfig: 'dev-staging',
-                frontGiftBucket: 'wedoogift-front-dev-staging-gift',
-                frontAdminBucket: 'wedoogift-front-dev-staging-admin',
-                notify: false
+                frontConfig: 'dev-staging'
         ],
         demo: [
                 beanstalkEnv: 'DEMO',
-                frontConfig: 'demo',
-                frontGiftBucket: 'wedoogift-front-demo-gift',
-                frontAdminBucket: 'wedoogift-front-demo-admin',
-                notify: true
+                frontConfig: 'demo'
         ],
         prod: [
                 beanstalkEnv: 'PRODUCTION',
-                frontConfig: 'prod',
-                frontGiftBucket: 'wedoogift-front-app-gift',
-                frontAdminBucket: 'wedoogift-front-app-admin',
-                notify: true
+                frontConfig: 'prod'
         ]
 ]
